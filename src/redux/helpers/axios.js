4540cb23ef080bbd8cc124c2d22b57275d3487e4
Berke <zberke97@gmail.com>
2025-08-31 22:51:13 +0300

    refactor: Externalize API base URL to an environment variable

--- src/redux/helpers/axios.js
+++ src/redux/helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://68a597872a3deed2960e07fa.mockapi.io/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default instance;
